71ad9c27363962a4df69e016ba574007e4278e19
Martin Liu <martinliu@lyft.com>
2021-04-24 19:27:23 -0700

    1.0.10: refine code

--- linkedinForDaisy/linkedinForDaisy.js
+++ linkedinForDaisy/linkedinForDaisy.js
@@ -2,7 +2,7 @@
 // @name       Daisy-Linkedin_Connecting_Script
 // @namespace  http://martin-liu.github.io/
 // @updateURL  https://raw.githubusercontent.com/martin-liu/mUserScripts/master/linkedinForDaisy/linkedinForDaisy.js
-// @version    1.0.9
+// @version    1.0.10
 // @description  Linkedin connecting script for Daisy Chu
 // @match      http*://*.linkedin.com/*
 // @copyright  2014+, Martin Liu
@@ -173,6 +173,11 @@
           if (inviteButton) {
             inviteButton.disabled = false;
             inviteButton.click();
+            await wait(300);
+            let gotItButton = [].find.call($(`${modalCls} button`), d => d.innerText=='Got it');
+            if (gotItButton) {
+              gotItButton.click();
+            }
           }
         }
       }
